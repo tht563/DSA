ce6fe4e33adedfd7344735951c6c6c4115ce12d9
Hung Tran <78260799+tht563@users.noreply.github.com>
2023-12-09 14:20:00 +1100

    Add files via upload

--- Algorithm/Main.java
+++ Algorithm/Main.java
@@ -1,10 +1,20 @@
 import week3.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		//Monotone
+		int[] A = {10,20,30,25};
+		System.out.println( Monotone.solution(A));
 		
-		int[] A = {2,2,2,2,1,2,-1,2,1,3};
-		Monotone.solution(A);
+		//StrinMisspelling
+		String S = "nice";
+		String T = "niece";
+		System.out.println(StringMisspelling.solution(S,T));
+		
+		//PublicTransportTickets
+		int[] a = {1,4,6,7,8,20};
+		System.out.println(PublicTransportTickets.solution(a));
 	}
 
 }
